d11fdab871b9569658a4a87f716aebeabd2b250d
raaj2045 <raaj2045@gmail.com>
2020-02-24 04:39:58 +0530

    react files added

---
+++ client/src/OtherPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+	return (
+		<div>
+			In some other page<Link to='/'>Go back to home</Link>
+		</div>
+	);
+};
